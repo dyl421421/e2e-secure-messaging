aa3109a1896167a3bc4176c90b2b52a4915fb95c
dyl421421 <dhokeefe421@gmail.com>
2020-05-10 14:00:36 -0400

    Added some more passport logic

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var session = require("express-session");
+const db = require("./modules/db-module");
+const bcrypt = require('node-bcrypt');
 
-const passport = require('passport');
+const passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
 const cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-app.use(passport.initialize(undefined));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +24,47 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: "351" }));
+// noinspection JSCheckFunctionSignatures
+app.use(passport.initialize());
+// noinspection JSCheckFunctionSignatures
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        db.query("SELECT id, username, passwordHash, passwordSalt, publicKey FROM users WHERE username=$1 OR email=$1", [username], (err, result) => {
+            if (err) {
+                createError(err);
+                return done(err);
+            }
+            if(result.rows.length > 0) {
+                const first = result.rows[0];
+
+                let hash = bcrypt.hashpw(password, first.passwordSalt);
+                if (hash === first.passwordHash) {
+                    done(null, { id: first.id, username: first.username, publicKey: first.publicKey});
+                } else {
+                    done(null, false, { message: "Incorrect password" } );
+                }
+            } else {
+                done(null, false, { message: "Incorrect username or email"});
+            }
+        });
+
+    }
+));
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+passport.deserializeUser((id, cb) => {
+    db.query('SELECT id, username, type FROM users WHERE id = $1', [parseInt(id, 10)], (err, results) => {
+        if(err) {
+            createError('Error when selecting user on session deserialize', err)
+            return cb(err)
+        }
+        cb(null, results.rows[0])
+    })
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
